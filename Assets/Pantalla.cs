2fc042f92b2018cee9c3280d88a017f3e6b491ca
marcosvissio <marcos.vissioQgmail.com>
2018-08-19 18:40:52 -0300

    First Commit

---
+++ Assets/Pantalla.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using UnityEngine.Video;
+
+public class Pantalla : MonoBehaviour, ITrackableEventHandler
+{
+    protected TrackableBehaviour mTrackableBehaviour;
+    public VideoPlayer pantallaVideo;
+    // Use this for initialization
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+    }
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED || newStatus == TrackableBehaviour.Status.TRACKED || newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            print("Detecta");
+            pantallaVideo.Play();
+        }
+        else if (previousStatus == TrackableBehaviour.Status.TRACKED && newStatus == TrackableBehaviour.Status.NO_POSE)
+        {
+            print("Pierde");
+            pantallaVideo.Pause();
+
+        }
+        else
+        {
+            print("Comienzo");
+            pantallaVideo.Stop();
+        }
+    }
+}
